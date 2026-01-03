72e9811d539509d44ce787eadd9617a9bce28fcd
Jinwoo Han <qew8502@gmail.com>
2026-01-03 18:55:37 +0900

    feat: print error message with command name

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  std::string command;
+
   std::cout << "$ ";
+  std::cin >> command;
+
+  std::cout << command << ": command not found";
 }
