c9be8e55c780a36da616e345dce13f87d7d3a698
Jinwoo Han <qew8502@gmail.com>
2026-01-07 21:04:20 +0900

    fix(build): support Windows build without readline

--- your_program.sh
+++ your_program.sh
@@ -14,7 +14,7 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
   cmake --build ./build
 )
 
@@ -22,4 +22,11 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec $(dirname "$0")/build/shell "$@"
+case "$(uname -s)" in
+  MINGW*|MSYS*|CYGWIN*)
+    exec "$(dirname "$0")/build/Debug/shell.exe" "$@"
+    ;;
+  *)
+    exec "$(dirname "$0")/build/shell" "$@"
+    ;;
+esac
